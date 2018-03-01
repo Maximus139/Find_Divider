487514ef6c59a2624afa0a34752513a4d70649ad
Maximus139 <myrona77795@gmail.com>
2018-03-01 15:17:27 +0200

    First commit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void find_divider (int N, int *prime_numbers, bool *result, int i)
         result[N] = true;
         while(prime_numbers[i])
         {
-            if (!(N % prime_numbers[i]))                          //если это делитель
+            if (!(N % prime_numbers[i]))                          //if it is divider
             {
                 int divider = N / prime_numbers[i];
                 find_divider(divider, prime_numbers, result, i);
@@ -27,35 +27,35 @@ void Eratrosphen (int N)
     for (int i = 0; i <= N; i++)
         result[i] = false;
     result[1] = true;
-    if (!(N % 2))                //если число парное
+    if (!(N % 2))                //if the number is even
     {
         prime_numbers[iter] = 2;
         result[2] = true;
         iter++;
     }
     for (int i = 3; i <= sqrtN; i += 2)
-        arr[i] = i;                              //простым делителем может быть только нпарное число, кроме 2
+        arr[i] = i;                              //prime divider can be only odd number, except 2
     for (int i = 3; i <= sqrtN; i += 2)
     {
         if (arr[i])
             {
-               if (!(N % arr[i])) {                //если это делитель
+               if (!(N % arr[i])) {                //if it is divider
                     prime_numbers[iter] = arr[i];
                     result[arr[i]] = true;
                     iter++ ;
                }
-                for (int j = i * i; j <= sqrtN; j += 2 * i){   //если найден простой делитель, все остальные делители
-                     arr[j] = 0;                               //что он образует, удаляем
+                for (int j = i * i; j <= sqrtN; j += 2 * i){   //if the prime number if found
+                     arr[j] = 0;                               //delete all other dividers, that it creates
                      }
             }
     }
-    if (!iter){                                                //проверка на ввод простого числа
+    if (!iter){                                                //check if input is prime number
         cout << "This is prime number" << endl;
         return;
     }
-    prime_numbers[iter] = 0;                                   //остановка рекурсии, когда простой делитель равен 0
+    prime_numbers[iter] = 0;                                   //the recursion stops when the prime number equals to 0
     find_divider(N, prime_numbers, result, 0);
-    for (int i = 0; i <= N; i++)                               //вывод
+    for (int i = 0; i <= N; i++)                               //output
         if (result[i]) cout << i << " ";
 }
 
